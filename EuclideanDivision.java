598ad2ebacd033756274e7420f2cbac0a52c16a3
Laina <lainalynvalena@gmail.com>
2025-09-02 20:11:49 +0000

    created euclideandivision.java

---
+++ EuclideanDivision.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Euclidean Division {
+    public static void main(String[] args) {
+        // Scanner object to receive keyboard input
+        Scanner input = new Scanner(System.in);
+        
+        // Prompt the user to enter an integer
+        System.out.print("Enter an integer: ");
+        int num = input.nextInt();
+
+        // Display the number
+        System.out.println("The number is: " + num);
+    }
+}
