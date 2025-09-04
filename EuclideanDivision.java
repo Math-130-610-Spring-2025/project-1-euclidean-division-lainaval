ae4e863f7f362edb7ee184dde32866f696c1e822
Laina <lainalynvalena@gmail.com>
2025-09-04 20:07:26 +0000

    created euclidean division.java

--- EuclideanDivision.java
+++ EuclideanDivision.java
@@ -1,15 +1,24 @@
-import java.util.Scanner;
-
-public class Euclidean Division {
+/**
+ * @author Lainalyn Valena
+ */
+public class EuclideanDivision {
     public static void main(String[] args) {
-        // Scanner object to receive keyboard input
-        Scanner input = new Scanner(System.in);
-        
-        // Prompt the user to enter an integer
-        System.out.print("Enter an integer: ");
-        int num = input.nextInt();
 
-        // Display the number
-        System.out.println("The number is: " + num);
+        System.out.print("Enter a dividened");
+        int dividened = input.nextInt();
+
+        System.out.println("Enter divisor");
+        int divisor = input.nextInt();
+
+        System.out.println(dividened);
+        System.out.println(divisor);
+        int quotient = dividened / divisor;
+        int remainder = dividened % divisor;
+
+        System.out.println(remainder);
+        System.out.println(quotient);
+        System.out.println(dividened + "=" divisor + "*" + quoutient + "+" remainder);
+
+        input.close();
     }
 }
